1a927e8d52581d1d831926c12076baed42103a30
mohsen-pashootan <64013260+mohsen-pashootan@users.noreply.github.com>
2021-08-10 21:30:42 +0430

    filter functionality

--- components/events/events-search.js
+++ components/events/events-search.js
@@ -1,20 +1,31 @@
+import { useRef } from "react";
 import Button from "../ui/button";
 import classes from "./events-search.module.css";
 
 function EventsSearch(props) {
+  const yearInputRef = useRef();
+  const monthInputRef = useRef();
+  const submitHandler = (event) => {
+    // event.preventDefault();
+    console.log("##");
+    const selectedYear = yearInputRef.current.value;
+    const selectedMonth = monthInputRef.current.value;
+
+    props.onSearch(selectedYear, selectedMonth);
+  };
   return (
-    <from className={classes.form}>
+    <from className={classes.form} onSubmit={submitHandler}>
       <div className={classes.controls}>
         <div className={classes.control}>
           <label htmlFor="year">Year</label>
-          <select id="year">
+          <select id="year" ref={yearInputRef}>
             <option value="2021">2021</option>
             <option value="2021">2022</option>
           </select>
         </div>
         <div className={classes.control}>
           <label htmlFor="month">Month</label>
-          <select id="month">
+          <select id="month" ref={monthInputRef}>
             <option value="1">January</option>
             <option value="2">February</option>
             <option value="3">March</option>
@@ -30,7 +41,7 @@ function EventsSearch(props) {
           </select>
         </div>
       </div>
-      <Button>Find Events </Button>
+      <Button>Find Events</Button>
     </from>
   );
 }
--- pages/events/index.js
+++ pages/events/index.js
@@ -1,14 +1,21 @@
+import { Fragment } from "react";
+import { useRouter } from "next/dist/client/router";
 import { getAllEvents } from "../../dummy-data";
 import EventList from "../../components/events/eventList";
 import EventsSearch from "../../components/events/events-search";
-import { Fragment } from "react";
 
 function AllEventsPage() {
+  const router = useRouter();
   const events = getAllEvents();
+
+  const findEventsHandler = (year, month) => {
+    const fullPath = `/event/${year}/${month}`;
+    router.push(fullPath);
+  };
 
   return (
     <Fragment>
-      <EventsSearch />
+      <EventsSearch onSearch={findEventsHandler} />
       <EventList items={events} />
     </Fragment>
   );
